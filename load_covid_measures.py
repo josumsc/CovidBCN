954fed63fb06605dcb354e5762a8b8f9e1b250e6
josumsc <josu93alonso@gmail.com>
2020-07-05 19:27:29 +0200

    Adding docker MakeFile and compose file, as well as other Airflow folders

--- load_covid_measures.py
+++ load_covid_measures.py
@@ -12,41 +12,83 @@ from sqlalchemy import create_engine
 with open('variables.json') as file:
     variables = json.load(file)
 
-# Where to get the data of the resource
-url = 'https://opendata-ajuntament.barcelona.cat/data/api/action/datastore_search?resource_id=290eb517-e7fa-41fb-aa59-389becb8f55b&limit=100000000'
+database_name = variables['sqlite_database_name']
+table_name = variables['covid_measures_table']['name']
+url = variables['covid_measures_table']['url']
 
-r = requests.get(url)
-assert r.status_code == 200, "Request was not succesful, Status Code: {code}".format(code=r.status_code)
+def get_request(url):
+    """
+    Fulfills a HTTP request to fetch the data from the API
+    :param url: URL of the resource to get
+    :return: Request object with the data fetched
+    """
+    r = requests.get(url)
+    assert r.status_code == 200, "Request was not succesful, Status Code: {code}".format(code=r.status_code)
+    return r
 
 
 # Decapsulating the json object into a DataFrame object
-bcn_covid_measures = r.json()['result']['records']
-bcn_covid_df = pd.DataFrame(bcn_covid_measures)
-bcn_covid_df = bcn_covid_df[['_id','Data_Indicador', 'Font', 'Frequencia_Indicador', 'Nom_Indicador',
-                             'Nom_Variable', 'Territori', 'Unitat', 'Valor']].set_index('_id')
+def request_to_df(r):
+    """
+    Transforms our request into a Pandas' DataFrame object
+    :param r: Request object with the data
+    :return: DataFrame with the fetched data
+    """
+    bcn_covid_measures = r.json()['result']['records']
+    bcn_covid_df = pd.DataFrame(bcn_covid_measures)
+    bcn_covid_df = bcn_covid_df[['_id','Data_Indicador', 'Font', 'Frequencia_Indicador', 'Nom_Indicador',
+                                 'Nom_Variable', 'Territori', 'Unitat', 'Valor']].set_index('_id')
+    return bcn_covid_df
 
-database_name = variables['sqlite_database_name']
 
 # If the DDBB do not exist in our WD we create it
-if database_name not in os.listdir():
-    print('Creating Database object:')
-    
-    conn = sqlite3.connect(database_name)
-    conn.close()
-    
-    assert database_name in os.listdir(), "Database could not be created."
-    print(f"Database {database_name} created succesfully")
-    
-else:
-    print(f"{database_name} already in directory")
+def is_database_in_wd(database_name):
+    """
+    Checks if the database exists in our working directory, and if not creates it
+    :param database_name: Database to look for
+    :return: None
+    """
+    if database_name not in os.listdir():
+        print('Creating Database object:')
 
+        conn = sqlite3.connect(database_name)
+        conn.close()
 
-table = variables['covid_measures_table']
-engine = create_engine(f"sqlite:///{database_name}")
+        assert database_name in os.listdir(), "Database could not be created."
+        print(f"Database {database_name} created succesfully")
+    else:
+        print(f"{database_name} already in directory")
 
-try:
-    bcn_covid_df.to_sql(table,
-                        engine,
-                        if_exists='replace')
-except Exception:
-    raise Exception(f"Couldn't write pandas' DataFrame into table {table} of {database_name}")
+    return None
+
+def dataframe_to_sql(df,database_name,table_name):
+    """
+    Writes our DataFrame to a table in our SQL Database
+    :param df: DataFrame to import in SQL
+    :param database_name: Database in which we should dump the data
+    :param table_name: Name of the table to dump the data into
+    :return: None
+    """
+    engine = create_engine(f"sqlite:///{database_name}")
+
+    try:
+        df.to_sql(table_name,
+                  engine,
+                  if_exists='replace')
+    except Exception:
+        raise Exception(f"Couldn't write pandas' DataFrame into table {table_name} of {database_name}")
+    return None
+
+def main():
+    """
+    Main function of the script. Combines previous steps.
+    :return: None
+    """
+    r = get_request(url)
+    df = request_to_df(r)
+    is_database_in_wd(database_name)
+    dataframe_to_sql(df,database_name,table_name)
+    return None
+
+if __name__ == '__main__':
+    main()
